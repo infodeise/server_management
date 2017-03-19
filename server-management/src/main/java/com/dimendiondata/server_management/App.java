16256a7c09e9b71e32abeb2f85bacaad8f19e4fd
Deise Miranda Borges <Deise Miranda Borges>
2017-03-19 14:26:46 +0000

    Starting server_management project

---
+++ server-management/src/main/java/com/dimendiondata/server_management/App.java
@@ -0,0 +1,13 @@
+package com.dimendiondata.server_management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
